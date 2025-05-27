bc4e3d9b87a857dcc7bab88e45a3504fad88f8c0
shuyshuys <ivlyns@protonmail.com>
2025-05-27 14:05:05 +0700

    add top navigation and include Zabbix and Host widgets in MonitoringPanelProvider

--- app/Providers/Filament/MonitoringPanelProvider.php
+++ app/Providers/Filament/MonitoringPanelProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\ZabbixResource\Widgets\ZabbixWidget;
+use App\Filament\Widgets\HostWidgets;
+use App\Models\Zabbix;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -27,6 +30,7 @@ class MonitoringPanelProvider extends PanelProvider
             ->id('monitoring')
             ->path('monitoring')
             ->login()
+            ->topNavigation(true)
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -38,7 +42,9 @@ class MonitoringPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                HostWidgets::class,
+                ZabbixWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
