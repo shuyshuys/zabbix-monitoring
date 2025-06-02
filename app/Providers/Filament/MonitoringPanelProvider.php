4b71595025d577c0e78e04790a72aaeeb8acef12
shuyshuys <ivlyns@protonmail.com>
2025-06-02 21:28:21 +0700

    add navigation item for Zabbix login with external link

--- app/Providers/Filament/MonitoringPanelProvider.php
+++ app/Providers/Filament/MonitoringPanelProvider.php
@@ -2,24 +2,25 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Resources\ZabbixResource\Widgets\ZabbixWidget;
-use App\Filament\Widgets\HostWidgets;
-use App\Models\Zabbix;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
+use Filament\Widgets;
+use App\Models\Zabbix;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
+use App\Filament\Widgets\HostWidgets;
+use Filament\Navigation\NavigationItem;
+use Filament\Http\Middleware\Authenticate;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Filament\Http\Middleware\AuthenticateSession;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use App\Filament\Resources\ZabbixResource\Widgets\ZabbixWidget;
 
 class MonitoringPanelProvider extends PanelProvider
 {
@@ -59,6 +60,11 @@ class MonitoringPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->navigationItems([
+                NavigationItem::make('Login to Zabbix')
+                    ->url(env('ZABBIX_LINK'), shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-link')
             ]);
     }
 }
