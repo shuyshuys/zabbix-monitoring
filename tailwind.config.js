2f7c22fdf0b99eb95443496568d95ba2a3d9e699
shuyshuys <ivlyns@protonmail.com>
2025-06-16 22:40:58 +0700

    feat: add Tailwind CSS configuration and theme file for styling enhancements

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './vendor/jaocero/filachat/resources/views/**/**/*.blade.php',
     ],
 }
