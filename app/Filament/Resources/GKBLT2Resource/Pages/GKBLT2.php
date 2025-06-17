8a20247a1eb6eb907227ced0463b54deb92b9a1f
shuyshuys <ivlyns@protonmail.com>
2025-06-17 23:04:32 +0700

    Add traffic monitoring widgets for Interface Ether3 to Ether8

--- app/Filament/Resources/GKBLT2Resource/Pages/GKBLT2.php
+++ app/Filament/Resources/GKBLT2Resource/Pages/GKBLT2.php
@@ -9,6 +9,14 @@ use App\Filament\Resources\GKBLT2Resource\Widgets\CpuChart;
 use App\Filament\Resources\GKBLT2Resource\Widgets\IcmpPingChart;
 use App\Filament\Resources\GKBLT2Resource\Widgets\MemoryChart;
 use App\Filament\Resources\GKBLT2Resource\Widgets\DhcpLeaseCountWidgets;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther1Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther2Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther3Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther4Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther5Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther6Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther7Chart;
+use App\Filament\Resources\GKBLT2Resource\Widgets\InterfaceEther8Chart;
 use Filament\Resources\Pages\Page;
 
 class GKBLT2 extends Page
@@ -28,6 +36,15 @@ class GKBLT2 extends Page
 
             IcmpPingChart::class,
             LinkStatusChart::class,
+
+            InterfaceEther1Chart::class,
+            InterfaceEther2Chart::class,
+            InterfaceEther3Chart::class,
+            InterfaceEther4Chart::class,
+            InterfaceEther5Chart::class,
+            InterfaceEther6Chart::class,
+            InterfaceEther7Chart::class,
+            InterfaceEther8Chart::class,
         ];
     }
 
