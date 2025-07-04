0cd58fb165dd7018318a06b34b6db3f678dd464b
shuyshuys <ivlyns@protonmail.com>
2025-07-04 18:08:58 +0700

    refactor: Clean up unused webhook routes and comments in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
-// use Marjose123\FilamentWebhookServer\Http\Controllers\WebhookController;
-// use Tapp\WebhookClient\Http\Controllers\WebhookController;
 
 Route::get('/', function () {
     return redirect()->route('filament.monitoring.auth.login');
 });
 
-Route::webhooks('webhook-receiving-url');
-
 Route::post('/webhooks/receive', [WebhookController::class, 'handle']);
-
-// Route::get('/monitoring/reports/device-status/download', [::class, 'downloadPdf'])
-//     ->name('filament.resources.report-resource.pages.device-status-report.download-pdf');
